c889b24fbce596afb7a536ac5240a3bbdb35105c
johan0A <arnaudchesse@outlook.com>
2024-05-10 03:04:39 +0200

    changed temporary test to match implementation

--- src/root.zig
+++ src/root.zig
@@ -6,9 +6,11 @@ test "QuadTree tests" {
     // const quad_tree = QuadTree(comptime Leaf: type, comptime Context: type, comptime equalLeafFn: fn(leaf1:Leaf, leaf2:Leaf, context:Context)bool)
     const allocator = testing.allocator;
     var item = std.ArrayList(u32).init(allocator);
-    var quad_tree = QuadTree(@TypeOf(item)).init(allocator);
+    var quad_tree = QuadTree(@TypeOf(item), null).init(allocator);
+    defer quad_tree.deinit();
+    var timer = try std.time.Timer.start();
     _ = try quad_tree.insert(&item, .{ 0, 0 });
-    _ = try quad_tree.insert(&item, .{ 2, 2 });
-    _ = try quad_tree.insert(&item, .{ 32000, 32000 });
-    try quad_tree.display();
+    _ = try quad_tree.insert(&item, .{ 50, 50 });
+    std.debug.print("Insertion time: {} \n", .{@as(f64, @floatFromInt(timer.read())) / 1_000_000_000});
+    try quad_tree.print_as_grid();
 }
